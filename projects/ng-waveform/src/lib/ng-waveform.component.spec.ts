4100ca1d71d98d35df7de7561b0aa3fe7e13d830
Jake Renzella <j@kerenzella.com>
2020-09-25 19:21:25 +1000

    mend

--- projects/ng-waveform/src/lib/ng-waveform.component.spec.ts
+++ projects/ng-waveform/src/lib/ng-waveform.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NgWaveformComponent } from './ng-waveform.component';
 
@@ -6,7 +6,7 @@ describe('NgWaveformComponent', () => {
   let component: NgWaveformComponent;
   let fixture: ComponentFixture<NgWaveformComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ NgWaveformComponent ]
     })
